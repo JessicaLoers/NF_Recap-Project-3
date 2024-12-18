a2d85d8456c768b188e7b0e9766d14ec07cbe204
Jessica Lörs <jessica.loers@gmail.com>
2024-12-18 12:07:03 +0100

    remove comments from task 2

--- index.js
+++ index.js
@@ -14,17 +14,11 @@ const page = 1;
 const searchQuery = '';
 
 async function fetchCharacters() {
-  // Fetch data from the Rick and Morty API for a specific page
   const result = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
-  // Parse the JSON response to get the data object
   const data = await result.json();
-  // Extract the list of characters from the "results" field of the API response
   const characters = data.results;
-  // Clear the cardContainer to remove any previous content before adding new character cards
   cardContainer.innerHTML = '';
-  // Create cards for each character
   const cards = characters.map(CharacterCard);
-  // Append each card to the container
   cards.forEach((card) => cardContainer.append(card));
 }
 

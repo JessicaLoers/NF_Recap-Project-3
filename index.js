b04e61763333238fc9ea8d4eb6bb05918eb3a72e
Jessica Lörs <jessica.loers@gmail.com>
2024-12-18 14:45:06 +0100

    add comments for students

--- index.js
+++ index.js
@@ -9,19 +9,30 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
+// change the maxPage value.
 const maxPage = 42;
+//Use "let" because the value of "page" will change as the user navigates between pages
 let page = 1;
 const searchQuery = '';
 
+// Add an event listener to the "Next" button for navigating to the next page
 nextButton.addEventListener('click', () => {
+  // If the current page is already the maximum page (42), do nothing
   if (page >= maxPage) return;
+
+  // Increment the current page
   page++;
+  // Fetch and display the characters for the new page
   fetchCharacters();
 });
 
+// Add an event listener to the "Previous" button for navigating to the next page
 prevButton.addEventListener('click', () => {
+  // If the current page is already the first page (1), do nothing
   if (page >= maxPage) return;
+  // Decrement the current page
   page--;
+  // Fetch and display the characters for the new page
   fetchCharacters();
 });
 

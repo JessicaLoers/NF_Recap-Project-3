5b20f2eda6495e82e8396bbb61511067392b8f59
Jessica Lörs <jessica.loers@gmail.com>
2024-12-18 14:02:47 +0100

    add next button functionalliy

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+const maxPage = 42;
+let page = 1;
 const searchQuery = '';
+
+nextButton.addEventListener('click', () => {
+  if (page >= maxPage) return;
+  page++;
+  fetchCharacters();
+});
 
 async function fetchCharacters() {
   const result = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);

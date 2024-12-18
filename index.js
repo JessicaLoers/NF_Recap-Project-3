3313a54b320d2ad7abc5351259ad284b5d45cc20
Jessica Lörs <jessica.loers@gmail.com>
2024-12-18 14:50:34 +0100

    remove comments from task 3

--- index.js
+++ index.js
@@ -9,28 +9,19 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 const maxPage = 42;
-//Use "let" because the value of "page" will change as the user navigates between pages
 let page = 1;
 const searchQuery = '';
 
-// Add an event listener to the "Next" button for navigating to the next page
 nextButton.addEventListener('click', () => {
-  // If the current page is already the maximum page (42), do nothing
   if (page >= maxPage) return;
 
-  // Increment the current page
   page++;
-  // Fetch and display the characters for the new page
   fetchCharacters();
 });
 
-// Add an event listener to the "Previous" button for navigating to the next page
 prevButton.addEventListener('click', () => {
-  // If the current page is already the first page (1), do nothing
   if (page >= maxPage) return;
-  // Decrement the current page
   page--;
-  // Fetch and display the characters for the new page
   fetchCharacters();
 });
 
@@ -41,7 +32,6 @@ async function fetchCharacters() {
   cardContainer.innerHTML = '';
   const cards = characters.map(CharacterCard);
   cards.forEach((card) => cardContainer.append(card));
-  // Update the pagination display to show the current page and total number of pages
   pagination.textContent = `${page} / ${maxPage}`;
 }
 
